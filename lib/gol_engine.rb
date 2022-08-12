b81a94d32d27750d26650b0e87b8c4b070c9d449
lijunwei <ljw532344863@sina.com>
2022-08-12 22:13:40 +0800

    Initial commit

---
+++ lib/gol_engine.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gol_engine/version"
+
+module GolEngine
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/gol_engine_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe GolEngine do
+  it "has a version number" do
+    expect(GolEngine::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
