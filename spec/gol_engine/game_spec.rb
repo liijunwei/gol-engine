5d87bf51d63921d9c7cccd2205d94800a84d35be
lijunwei <ljw532344863@sina.com>
2022-08-14 12:27:23 +0800

    Add spec refercing https://www.youtube.com/watch\?v\=IWY0WDEIWFI\&t\=22s

--- spec/gol_engine/game_spec.rb
+++ spec/gol_engine/game_spec.rb
@@ -4,4 +4,26 @@ RSpec.describe GolEngine::Game do
   it "responds to #tick" do
     expect(subject).to respond_to(:tick)
   end
+
+  describe "#tick" do
+    it "keeps dead cells with 0 alive neighbors"
+
+    it "kills alive cells with 0 alive neighbors"
+
+    it "keeps dead cells with 1 alive neighbors"
+
+    it "kills alive cells with 1 alive neighbors"
+
+    it "keeps dead cells with 2 alive neighbors"
+
+    it "keeps alive cells with 2 alive neighbors"
+
+    it "awakes dead cells with 3 alive neighbors"
+
+    it "keeps alive cells with 3 alive neighbors"
+
+    it "keeps dead cells with 4 alive neighbors"
+
+    it "kills alive cells with 4 alive neighbors"
+  end
 end
