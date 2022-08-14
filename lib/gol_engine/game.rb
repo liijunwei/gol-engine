2ad0e66e02823d8f88aafd0ccf287482e9386546
lijunwei <ljw532344863@sina.com>
2022-08-14 15:55:17 +0800

    Introduce world class without breaking any specs

--- lib/gol_engine/game.rb
+++ lib/gol_engine/game.rb
@@ -22,23 +22,5 @@ module GolEngine
         end
       end
     end
-
-    private
-
-    def active_neighbors(world, i, j)
-      [i - 1, i , i + 1].product([j - 1, j , j + 1])
-        .reject {|ni, nj| ni == i && nj == j}
-        .map {|ni, nj| fetch(world, ni, nj)}
-        .count(:alive)
-    end
-
-    def fetch(world, i, j)
-      return if i < 0
-      return if i >= world.size
-      return if j < 0
-      return if j >= world[i].size
-
-      return world[i][j]
-    end
   end
 end
