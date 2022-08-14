3c8aa657e4a0ffda59bff399db97a1d71d261dbd
lijunwei <ljw532344863@sina.com>
2022-08-14 11:14:55 +0800

    Commit manually

--- lib/gol_engine/game.rb
+++ lib/gol_engine/game.rb
@@ -2,6 +2,7 @@
 
 module GolEngine
   class Game
-
+    def tick
+    end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe GolEngine::Game do
-  it "responds to #tick"
+  it "responds to #tick" do
+    expect(subject).to respond_to(:tick)
+  end
 end
