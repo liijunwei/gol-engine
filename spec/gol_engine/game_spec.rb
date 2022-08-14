476b9180c2b800f50159d41159ab96a468121b0d
lijunwei <ljw532344863@sina.com>
2022-08-14 12:46:10 +0800

    Make second spec pass

--- spec/gol_engine/game_spec.rb
+++ spec/gol_engine/game_spec.rb
@@ -16,7 +16,64 @@ RSpec.describe GolEngine::Game do
         expect(subject.tick(world)[1][1]).to eq(:dead)
       end
 
-      it "dies with 1 alive neighbors(underpopulation)"
+      it "dies with 1 alive neighbors(underpopulation)" do
+        world1 = [
+                   [:dead, :dead,  :dead],
+                   [:alive, :alive, :dead],
+                   [:dead, :dead,  :dead]
+                 ]
+        expect(subject.tick(world1)[1][1]).to eq(:dead)
+
+        world2 = [
+                   [:alive, :dead,  :dead],
+                   [:dead, :alive, :dead],
+                   [:dead, :dead,  :dead]
+                 ]
+        expect(subject.tick(world2)[1][1]).to eq(:dead)
+
+        world3 = [
+                   [:dead, :alive,  :dead],
+                   [:dead, :alive, :dead],
+                   [:dead, :dead,  :dead]
+                 ]
+        expect(subject.tick(world3)[1][1]).to eq(:dead)
+
+        world4 = [
+                   [:dead, :dead,  :alive],
+                   [:dead, :alive, :dead],
+                   [:dead, :dead,  :dead]
+                 ]
+        expect(subject.tick(world4)[1][1]).to eq(:dead)
+
+        world5 = [
+                   [:dead, :dead,  :dead],
+                   [:dead, :alive, :alive],
+                   [:dead, :dead,  :dead]
+                 ]
+        expect(subject.tick(world5)[1][1]).to eq(:dead)
+
+        world6 = [
+                   [:dead, :dead,  :dead],
+                   [:dead, :alive, :dead],
+                   [:dead, :dead,  :alive]
+                 ]
+        expect(subject.tick(world6)[1][1]).to eq(:dead)
+
+        world7 = [
+                   [:dead, :dead,  :dead],
+                   [:dead, :alive, :dead],
+                   [:dead, :alive,  :dead]
+                 ]
+        expect(subject.tick(world7)[1][1]).to eq(:dead)
+
+        world8 = [
+                   [:dead, :dead,  :dead],
+                   [:dead, :alive, :dead],
+                   [:alive, :dead,  :dead]
+                 ]
+        expect(subject.tick(world8)[1][1]).to eq(:dead)
+      end
+
       it "survives with 2 alive neighbors"
       it "survives with 3 alive neighbors"
       it "dies with 4 alive neighbors(overpopulation)"
