5700186a68ae69a272beb6ffea48f24c37e0d321
lijunwei <ljw532344863@sina.com>
2022-08-14 13:48:23 +0800

    Make third spec pass

--- lib/gol_engine/game.rb
+++ lib/gol_engine/game.rb
@@ -8,6 +8,10 @@ module GolEngine
           case active_neighbors(world, i, j)
           when 0
             :dead
+          when 1
+            :dead
+          when 2
+            :alive
           end
         end
       end
@@ -16,7 +20,16 @@ module GolEngine
     private
 
     def active_neighbors(world, i, j)
-      0
+      [[0, j - 1], [0, j + 1]]
+        .map {|ni, nj| fetch(world, ni, nj)}
+        .count(:alive)
+    end
+
+    def fetch(world, i, j)
+      return if j < 0
+      return if j >= world[0].size
+
+      return world[i][j]
     end
   end
 end
